147b149112d39139243be44743647a285ff6eef8
Martin Erren <martin3r@Mac.fritz.box>
2025-09-21 17:30:43 +0200

    init

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use Platform\Okr\Livewire\CycleManagement;
 use Platform\Okr\Livewire\OkrManagement;
 
 // Dashboard
-Route::get('/', Dashboard::class)->name('dashboard');
+Route::get('/', Dashboard::class)->name('okr.dashboard');
 
 // OKR Management
-Route::get('/okrs', [OkrManagement::class, 'index'])->name('okrs.index');
-Route::get('/okrs/create', [OkrManagement::class, 'create'])->name('okrs.create');
-Route::get('/okrs/{okr}', [OkrManagement::class, 'show'])->name('okrs.show');
+Route::get('/okrs', [OkrManagement::class, 'index'])->name('okr.okrs.index');
+Route::get('/okrs/create', [OkrManagement::class, 'create'])->name('okr.okrs.create');
+Route::get('/okrs/{okr}', [OkrManagement::class, 'show'])->name('okr.okrs.show');
 
 // Cycle Management
-Route::get('/cycles', CycleManagement::class)->name('cycles.index');
-Route::get('/cycles/create', [CycleManagement::class, 'create'])->name('cycles.create');
-Route::get('/cycles/{cycle}', [CycleManagement::class, 'show'])->name('cycles.show');
+Route::get('/cycles', CycleManagement::class)->name('okr.cycles.index');
+Route::get('/cycles/create', [CycleManagement::class, 'create'])->name('okr.cycles.create');
+Route::get('/cycles/{cycle}', [CycleManagement::class, 'show'])->name('okr.cycles.show');
