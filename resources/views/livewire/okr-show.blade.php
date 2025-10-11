2432bfaf93f23a9e1556b4d5ed360c5878639522
Martin Erren <martin3r@Mac.fritz.box>
2025-10-11 18:38:05 +0200

    tailwind

--- resources/views/livewire/okr-show.blade.php
+++ resources/views/livewire/okr-show.blade.php
@@ -375,7 +375,7 @@
 
     {{-- Right Sidebar --}}
     <x-slot name="activity">
-        <x-ui-page-sidebar title="Aktivitäten" width="w-80" :defaultOpen="false">
+        <x-ui-page-sidebar title="Aktivitäten" width="w-80" side="right" :defaultOpen="false">
             <div class="p-6 space-y-6">
                 {{-- Recent Activities --}}
                 <div>
